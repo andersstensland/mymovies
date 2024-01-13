10e357123c8cda9f9f16ff803c020482e21bcfac
Anders Stensland <43165915+andersstensland@users.noreply.github.com>
2024-01-13 11:58:16 +0100

    Update firebase.js

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAJFV0zhW99QiyBaBj6bTl-gnn5BK0ubW0",
-  authDomain: "mymovies-b6f8b.firebaseapp.com",
-  projectId: "mymovies-b6f8b",
-  storageBucket: "mymovies-b6f8b.appspot.com",
-  messagingSenderId: "912054780129",
-  appId: "1:912054780129:web:3d0323ef47d9f59500b7a0",
-  measurementId: "G-SH120LPZTC",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
